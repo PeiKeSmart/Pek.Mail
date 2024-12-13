4152a87a81777a6b7d4882783dc683232947262b
猿人易 <qcjxberin@outlook.com>
2024-12-13 19:29:34 +0800

    添加邮箱配置类和包引用
    在 `Pek.Mail.csproj` 文件中：
    * 添加了对 `DH.NCore` 版本 `4.1.2024.1213-beta0858` 的包引用。
    * 添加了对 `MailKit` 版本 `4.9.0` 的包引用。
    
    在 `MailSettings.cs` 文件中：
    * 新增了 `System.ComponentModel` 和 `NewLife.Configuration` 的 using 语句。
    * 新增了 `Pek.Mail` 命名空间。
    * 新增了一个名为 `MailSettings` 的类，继承自 `Config<MailSettings>`，用于配置邮箱设置。
      * 该类包含一个 `MailList` 属性，用于存储邮箱列表。
    * 新增了一个名为 `MailData` 的类，用于表示邮箱数据。
      * 该类包含三个属性：`Host`（邮箱服务器地址）、`IsSSL`（是否使用 SSL 协议）和 `UserName`（用户名）。

---
+++ Pek.Mail/MailSettings.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+
+using NewLife.Configuration;
+
+namespace Pek.Mail;
+
+/// <summary>邮箱设置</summary>
+[DisplayName("邮箱设置")]
+[Config("Mail")]
+public class MailSettings : Config<MailSettings>
+{
+    /// <summary>
+    /// 邮箱列表
+    /// </summary>
+    [Description("邮箱列表")]
+    public IList<MailData>? MailList{ get; set; }
+}
+
+/// <summary>
+/// 邮箱数据
+/// </summary>
+public class MailData
+{
+    /// <summary>
+    /// 邮箱服务器的地址，如 smtp.exmail.qq.com
+    /// </summary>
+    public String? Host { get; set; }
+
+    /// <summary>
+    /// SSL协议
+    /// </summary>
+    public Boolean IsSSL { get; set; }
+
+    /// <summary>
+    /// SSL协议
+    /// </summary>
+    public Boolean UserName { get; set; }
+}
