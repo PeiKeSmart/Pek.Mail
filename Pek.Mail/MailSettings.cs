cc6b22220ae67159f0e4920c53e7c4ab5c1a3f32
猿人易 <qcjxberin@outlook.com>
2024-12-19 17:13:59 +0800

    更新 NuGet 源地址，改进 MailSettings 类
    更新 `publish-beta.yml` 和 `publish.yml` 文件中的 `dotnet nuget push` 命令源地址。
    在 `MailSettings.cs` 文件中，添加命名空间引用，重命名属性，添加构造函数和方法，扩展 `MailData` 类属性。
    更新 `Pek.Mail.csproj` 文件中的 `DH.NCore` 包版本，添加 `Pek.Common` 包引用。

--- Pek.Mail/MailSettings.cs
+++ Pek.Mail/MailSettings.cs
@@ -2,6 +2,8 @@
 
 using NewLife.Configuration;
 
+using Pek.Ids;
+
 namespace Pek.Mail;
 
 /// <summary>邮箱设置</summary>
@@ -10,10 +12,40 @@ namespace Pek.Mail;
 public class MailSettings : Config<MailSettings>
 {
     /// <summary>
-    /// 邮箱列表
+    /// 邮箱数据
     /// </summary>
-    [Description("邮箱列表")]
-    public IList<MailData>? MailList{ get; set; }
+    public IList<MailData>? Data { get; set; }
+
+    /// <summary>实例化</summary>
+    public MailSettings() { }
+
+    /// <summary>加载时触发</summary>
+    protected override void OnLoaded()
+    {
+        if (Data == null || Data.Count == 0)
+        {
+            var list = new List<MailData>
+            {
+                new() {
+                    Code = IdHelper.GetIdString(),
+                    Host = "smtp.qiye.aliyun.com",
+                    Port = 465,
+                    IsSSL = true,
+                    UserName = "service@hlktech.com",
+                    Password = "hOFvyjOcLDfrNvM1",
+                    From = "service@hlktech.com",
+                    FromName = "海凌科",
+                    IsEnabled = true,
+                    IsDefault = true,
+                    EmailSuffix = String.Empty
+                }
+            };
+
+            Data = list;
+        }
+
+        base.OnLoaded();
+    }
 }
 
 /// <summary>
@@ -22,17 +54,68 @@ public class MailSettings : Config<MailSettings>
 public class MailData
 {
     /// <summary>
-    /// 邮箱服务器的地址，如 smtp.exmail.qq.com
+    /// 惟一标识
     /// </summary>
+    [Description("惟一标识")]
+    public String? Code { get; set; }
+
+    /// <summary>
+    /// 邮箱服务器
+    /// </summary>
+    [Description("邮箱服务器")]
     public String? Host { get; set; }
 
     /// <summary>
-    /// SSL协议
+    /// 邮箱服务器端口
     /// </summary>
+    [Description("邮箱服务器端口")]
+    public Int32 Port { get; set; } = 25;
+
+    /// <summary>
+    /// 是否SSL协议
+    /// </summary>
+    [Description("是否SSL协议")]
     public Boolean IsSSL { get; set; }
 
     /// <summary>
-    /// SSL协议
+    /// 身份验证用户名
     /// </summary>
-    public Boolean UserName { get; set; }
+    [Description("身份验证用户名")]
+    public String? UserName { get; set; }
+
+    /// <summary>
+    /// 身份验证密码
+    /// </summary>
+    [Description("身份验证密码")]
+    public String? Password { get; set; }
+
+    /// <summary>
+    /// 发信人邮件地址
+    /// </summary>
+    [Description("发信人邮件地址")]
+    public String? From { get; set; }
+
+    /// <summary>
+    /// 发送邮箱昵称
+    /// </summary>
+    [Description("发送邮箱昵称")]
+    public String? FromName { get; set; }
+
+    /// <summary>
+    /// 是否启用
+    /// </summary>
+    [Description("是否启用")]
+    public Boolean IsEnabled { get; set; }
+
+    /// <summary>
+    /// 是否默认
+    /// </summary>
+    [Description("是否默认")]
+    public Boolean IsDefault { get; set; }
+
+    /// <summary>
+    /// 邮箱后缀，当包含有指定的邮箱后缀时，使用当前邮箱配置,不包含时使用默认邮箱配置
+    /// </summary>
+    [Description("邮箱后缀")]
+    public String? EmailSuffix { get; set; }
 }
